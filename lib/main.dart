557959c76fdf6eec06334d1d1748f91f3880dc55
mwyzer <muhammad.wyzer@gmail.com>
2024-01-08 11:50:26 +0700

    List

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -20,8 +19,6 @@ class MyApp extends StatelessWidget {
   //
   // print("hi there");
   // print("8 % 5");
-
-
 
   /*
 
@@ -85,12 +82,9 @@ class MyApp extends StatelessWidget {
       default:
     }
 
+       String grade = "F";
 
-  */
-
-  @override
-  Widget build(BuildContext context) {
-    String grade = "F";
+    E X A M P L E
 
     switch (grade) {
       case "A":
@@ -109,6 +103,141 @@ class MyApp extends StatelessWidget {
       default:
         print("Invalid level");
     }
+
+    F O R   L O O P
+
+    imagine you have a box of 10 different colored crayons and you want to draw
+    a circle with each one. Instead of saying:
+
+    "Draw a circle with the 1st crayon, then draw a circle with the 2nd crayon, etc.."
+
+    It's much easier to say:
+
+    "For each crayon in the box, draw a circle."
+
+    for ( initialization; condition; iteration ) {
+
+    }
+
+    break -> break out of loop
+    continue -> skip this current iteration
+
+     for (int i = 0; i <= 5; i++) {
+      if (i == 6) {
+        continue;
+      }
+      print(i);
+    }
+
+    if you don't know how many times to loop, you can use a while loop to keep
+    looping until a certain condition is met.
+
+    ----------------------------------------------------------------------------
+    F U N C T I O N S / M E T H O D S
+
+    We just looked at some cool blocks of code that gets things done!
+    We can organise these blocks of code into functions so that we can reuse
+    them easily.
+
+    'void' means that the function returns nothing. Shortly we will look at
+    functions that do return something. This one for now just executes the code
+    in the function.
+
+    // basic function
+    greet function
+    void greet() {
+      print("Hello, Wyzer!");
+    }
+
+    // function with params
+    void greetPerson(String.name{
+
+    }
+
+    int add(int a, int b) {
+      int sum = a + b;
+      return sum;
+    }
+
+
+
+    int mySum = add(2, 4);
+    print(mySum);
+
+    // D A T A   S T R U C T U R E S
+
+
+
+      List<dynamic> numbers = [1, 2, 3];
+  // numbers[0] -> 1
+  // numbers[1] -> 2
+  // numbers[2] -> 3
+
+  List<String> names= ["Mitch", "Koko", "Vince"];
+
+  void printNumbers() {
+    for (int i = 0; i < numbers.length; i++ ) {
+      print(numbers[i]);
+    }
+  }
+
+  void printNames() {
+    for (int i = 0; i < names.length; i++) {
+      print(names[i]);
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+  printNumbers();
+  printNames();
+
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
+
+
+
+
+  */
+
+  //
+  // void greet() {
+  //   print("Hello, Mitch!");
+  // }
+  //
+  // // function with params
+  // void greetPerson(String name) {
+  //   print("Hello " + name);
+  // }
+
+  List<dynamic> numbers = [1, 2, 3];
+  // numbers[0];
+
+  List<String> names= ["Mitch", "Koko", "Vince"];
+
+  void printNumbers() {
+    for (int i = 0; i < numbers.length; i++ ) {
+      print(numbers[i]);
+    }
+  }
+
+  void printNames() {
+    for (int i = 0; i < names.length; i++) {
+      print(names[i]);
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+  printNumbers();
+  printNames();
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
